7823633812b30c2416fd6924298a9f90d6458cd5
aichamps <130439811+aichamps@users.noreply.github.com>
2023-05-13 16:11:50 +0530

    Update xyzz.py

--- xyzz.py
+++ xyzz.py
@@ -6,7 +6,7 @@ from PIL import Image
 elementList=[]
 elementMatrix=[]
 
-st.set_page_config(layout="wide", page_icon="💬", page_title="CEB")
+st.set_page_config(layout="wide", page_icon="🧪⚗️", page_title="CEB")
 image = Image.open('logo-image.png')
 col1, col2 = st.columns([7, 3])
 with col1:
@@ -15,7 +15,7 @@ with col1:
     st.markdown("""
     AIChamps 🎯
     """)
-    st.header("................................................................")
+    st.header("__________________________________________________________________________________________________________________________________________________________")
     st.subheader("Hello Fellow Students!!!")
     st.text("Lets Balance our Chemical Equations here")
 with col2:
